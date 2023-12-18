235c88899a9566999b7455c309594489dcc98505
Oleg <codingoleg@proton.me>
2023-12-18 05:21:41 +0300

    Feat: Authentication and permissions

--- django_app/serialazers.py
+++ django_app/serialazers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
-from .models import Units, Users, Jobs
+from .models import Units, Jobs, Users
+
+
+class UserAuthSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 
 class UnitSerializer(serializers.ModelSerializer):
--- django_app/views.py
+++ django_app/views.py
@@ -1,12 +1,18 @@
-from django.db.models import Q, QuerySet
-from rest_framework import generics
-from rest_framework.permissions import IsAdminUser
-from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from django.db.models import Q
+from rest_framework import generics, status
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .models import Units, Users, Jobs
-from .serialazers import UnitSerializer, UserSerializer, JobSerializer
 from .db import create_test_tables, units
+from .models import Units, Users, Jobs
+from .serialazers import UnitSerializer, UserSerializer, JobSerializer, UserAuthSerializer
+from .utils import get_users_info
 
 
 class UnitsView(generics.ListAPIView):
@@ -22,55 +28,76 @@ class UnitRead(generics.RetrieveAPIView):
 class UnitCreate(generics.CreateAPIView):
     queryset = Units.objects.all()
     serializer_class = UnitSerializer
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
 
 
 class UnitUpdateDestroy(generics.UpdateAPIView, generics.DestroyAPIView):
     queryset = Units.objects.all()
     serializer_class = UnitSerializer
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
 
 
 class UsersView(generics.ListAPIView):
     queryset = Users.objects.all().order_by('id')
     serializer_class = UserSerializer
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
 
 
 class UserRead(generics.RetrieveAPIView):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
 
 
 class UserCreate(generics.CreateAPIView):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
 
 
 class UserUpdateDestroy(generics.UpdateAPIView, generics.DestroyAPIView):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
 
 
 class JobsView(generics.ListAPIView):
     queryset = Jobs.objects.all().order_by('id')
     serializer_class = JobSerializer
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
 
 
 class JobRead(generics.RetrieveAPIView):
     queryset = Jobs.objects.all()
     serializer_class = JobSerializer
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
 
 
 class JobCreate(generics.CreateAPIView):
     queryset = Jobs.objects.all()
     serializer_class = JobSerializer
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
 
 
 class JobUpdateDestroy(generics.UpdateAPIView, generics.DestroyAPIView):
     queryset = Jobs.objects.all()
     serializer_class = JobSerializer
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
 
 
 class CreateTestData(APIView):
     """Создание тестовых таблиц"""
+
     def get(self, request):
         create_test_tables()
 
@@ -79,44 +106,33 @@ class CreateTestData(APIView):
 
 class GetMyInfo(APIView):
     """Получение данных о себе"""
+
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request):
         user_id = request.GET.get('user')
         user = Users.objects.get(id=user_id)
         jobs = user.jobs_set.all().values()
         info = []
         for job in jobs:
-            info.append({"name": job['name'], "permission": job['permission'], "unit": job['unit']})
+            info.append({"username": job['jobname'], "permission": job['permission'], "unit": job['unit']})
 
         return Response(info)
 
 
-def get_users_info(info: list, jobs_records: QuerySet):
-    """Получение данных о пользователях"""
-    for unit_job in jobs_records:
-
-        # Получаем id пользователя по id должности
-        jobs_instance = Jobs.objects.get(id=unit_job.id)
-        users_instances = jobs_instance.user_jobs.all().values()
-
-        # Получаем все должности по id пользователя
-        user_instance = Users.objects.get(id=users_instances[0]['id'])
-        jobs_instances = user_instance.jobs_set.all().values()
-
-        # Добавляем имя пользователя в каждую запись с его должностью
-        for j in jobs_instances:
-            j['username'] = users_instances[0]['name']
-            info.append(j)
-
-    return info
-
-
 class GetMyUnitInfo(APIView):
-    """Получение данных о пользователях в своем подразделении. Только для прав доступа =<5."""
+    """Получение данных о пользователях в своем подразделении. Только для прав доступа от 5 и больше."""
+
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request):
         user_id = request.GET.get('user')
         user = Users.objects.get(id=user_id)
         jobs = user.jobs_set.all().values()
         info = []
+
         for job in jobs:
             if job['permission'] < 5:
                 info.append({"detail": f"Недостаточно прав доступа для отдела {units[job['unit']]}"})
@@ -130,6 +146,10 @@ class GetMyUnitInfo(APIView):
 class GetSubUnitInfo(APIView):
     """Получение данных о пользователях в своем и дочерних подразделенях.
     Только для головных подразделений, код которых кратен 100."""
+
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request):
         user_id = request.GET.get('user')
         user = Users.objects.get(id=user_id)
@@ -144,3 +164,32 @@ class GetSubUnitInfo(APIView):
                 get_users_info(info, jobs_records)
 
         return Response(info)
+
+
+@api_view(['POST'])
+def signup(request):
+    serializer = UserAuthSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+
+        return Response({'token': token.key, 'user': serializer.data})
+
+    return Response(serializer.errors, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def login(request):
+    user = get_object_or_404(User, username=request.data['username'])
+
+    if not user.check_password(request.data['password']):
+        return Response({"detail": "Пользователь не найден"})
+
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserAuthSerializer(instance=user)
+
+    return Response({'token': token.key, 'user': serializer.data})
