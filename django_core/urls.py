f4e6e75162e234fc074e92c2549d25175b81ca3e
Oleg <codingoleg@proton.me>
2023-12-18 05:17:23 +0300

    Rename 'drf_01' to 'django_core'

--- django_core/urls.py
+++ django_core/urls.py
@@ -1,26 +1,27 @@
 """
-URL configuration for drf_01 project.
+URL configuration for django_core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, username='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), username='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from django_app.views import UnitsView, UnitCreate, UnitRead, UnitUpdateDestroy
 from django_app.views import UsersView, UserCreate, UserRead, UserUpdateDestroy
 from django_app.views import JobsView, JobCreate, JobRead, JobUpdateDestroy
 from django_app.views import CreateTestData, GetMyInfo, GetMyUnitInfo, GetSubUnitInfo
+from django_app.views import signup, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -47,5 +48,9 @@ urlpatterns = [
     # Get info
     path('api/v1/get_my_info/', GetMyInfo.as_view()),
     path('api/v1/get_my_unit_info/', GetMyUnitInfo.as_view()),
-    path('api/v1/get_subunit_info/', GetSubUnitInfo.as_view())
+    path('api/v1/get_subunit_info/', GetSubUnitInfo.as_view()),
+
+    # Auth
+    re_path('signup/', signup),
+    re_path('login/', login),
 ]
